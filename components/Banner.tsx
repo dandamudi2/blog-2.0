d02075488eff716015da6e2c86094cdb503b68c4
dandamudi2 <srinivasd2mail@gmail.com>
2023-03-03 13:40:18 +0800

    banner changes

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
     <div >
-         <h1 className="text-7xl">Sonny's Daily Blog</h1> 
+         <h1 className="text-7xl">Sonny&aposs Daily Blog</h1> 
          <h2>Welcome to {" "}
          <span className="underline decoration-4 decoration-[#F7AB0A]">
             Every Developers
